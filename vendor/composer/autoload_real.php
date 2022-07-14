123c7fed6611d50e98d7a742904b61e6ab6a8bc2
ayezinzinaung <AyeZin2Aung101120@gmail.com>
2022-07-14 15:04:08 +0630

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,11 +23,30 @@ class ComposerAutoloaderInit3a824fb7fdea2c6aad7340a100dd2a8f
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit3a824fb7fdea2c6aad7340a100dd2a8f', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit3a824fb7fdea2c6aad7340a100dd2a8f', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInit3a824fb7fdea2c6aad7340a100dd2a8f::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInit3a824fb7fdea2c6aad7340a100dd2a8f::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'eb63bc6019e40b0cd145a8d15c5848267a975140',
+        'name' => 'lenovo/se',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lenovo/se' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'eb63bc6019e40b0cd145a8d15c5848267a975140',
+            'dev_requirement' => false,
+        ),
+    ),
+);
